0de1a0043fdbe7a52fae372b1d69f07f558dea8e
Ebube-Ochemba <ebube17@gmail.com>
2024-07-23 17:24:25 +0100

    deleted

--- 0x00-ES6_basics/10-loops.js
+++
@@ -1,7 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-    let newArray = [];
-    for (let value of array) {
-      newArray.push(appendString + value);
-    }
-    return newArray;
-  }
--- 0x00-ES6_basics/__tests__/1-block-scoped.test.js
+++
@@ -1,9 +0,0 @@
-import taskBlock from '../1-block-scoped';
-
-test('taskBlock should return [false, true] when trueOrFalse is true', () => {
-    expect(taskBlock(true)).toEqual([false, true]);
-});
-
-test('taskBlock should return [false, true] when trueOrFalse is false', () => {
-    expect(taskBlock(false)).toEqual([false, true]);
-});
