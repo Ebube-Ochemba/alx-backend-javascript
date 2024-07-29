0c7b3ae51a496c4ba2013e0497cb8ee470787036
Ebube-Ochemba <ebube17@gmail.com>
2024-07-29 10:25:23 +0100

    refactor

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,4 +1,6 @@
 // 5-photo-reject.js
 export default function uploadPhoto(filename) {
-  return Promise.reject(new Error(`${filename} cannot be processed`));
+  return Promise((resolve, reject) => {
+    reject(new Error(`${filename} cannot be processed`));
+  });
 }
