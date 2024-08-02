c1ffbd89f283da35b5dcc3fa089024d233d36328
Ebube-Ochemba <ebube17@gmail.com>
2024-08-03 00:10:05 +0100

    declaration and definition

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,11 @@
+// interface for task_3
+
+// type RowID
+export type RowID = number;
+
+// Interface for row elements
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
