f77a58d28374d5fd7ac1949b69a1a4c3c77b6bbd
Ebube-Ochemba <ebube17@gmail.com>
2024-09-18 16:31:38 +0100

    better comments

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,16 @@
+// Prompt the user for their name
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen to stdin
+// Listen to stdin for input using the 'readable' event
 process.stdin.on('readable', () => {
+  // Read the chunk of input provided by the user
   const chunk = process.stdin.read();
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
-// listens only for automatic close (by piping)
+// Listen for the 'end' event, which triggers when input stream closes (e.g., by piping)
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
