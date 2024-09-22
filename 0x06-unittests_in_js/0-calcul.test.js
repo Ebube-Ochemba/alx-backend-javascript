aeede676c2e04e8638bcc8af349ae92f3d3440aa
Ebube-Ochemba <ebube17@gmail.com>
2024-09-22 16:19:18 +0100

    added zero and negative test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,22 +3,32 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
   it('should return 4 when inputs are 1 and 3', function () {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+    const res = calculateNumber(1, 3);
+    assert.strictEqual(res, 4);
   });
 
-  it('should return 5 when inputs are 1 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it('checks case for first number rounded', function () {
+    const res = calculateNumber(1.2, 3);
+    assert.strictEqual(res, 4);
   });
 
-  it('should return 4 when inputs are 1.2 and 3', function () {
-    assert.strictEqual(calculateNumber(1.2, 3), 4);
+  it('checks case for second number rounded', function () {
+    const res = calculateNumber(1, 3.7);
+    assert.strictEqual(res, 5);
   });
 
-  it('should return 5 when inputs are 1.2 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  it('checks case for both numbers rounded', function () {
+    const res = calculateNumber(1.2, 3.7);
+    assert.strictEqual(res, 5);
   });
 
-  it('should return 6 when inputs are 1.5 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it('checks case for zero', function () {
+    const res = calculateNumber(0.0, 0.0);
+    assert.strictEqual(res, 0);
+  });
+
+  it('checks case for both negative numbers rounded', function () {
+    const res = calculateNumber(-2.7, -3.8);
+    assert.strictEqual(res, -7);
   });
 });
